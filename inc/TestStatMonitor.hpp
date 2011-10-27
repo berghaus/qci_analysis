2a9cb961d7a1668f9d4f37e92f9f33bbe8ab9183
Frank Berghaus <frank@uvic.ca>
2011-10-27 16:19:00 -0700

    created some histograms as guts for the TestStatMonitor

--- inc/TestStatMonitor.hpp
+++ inc/TestStatMonitor.hpp
@@ -1,8 +1,10 @@
 #ifndef TEST_STAT_MONITOR_HPP
 #define TEST_STAT_MONITOR_HPP
+#include <map>
 #include <string>
 
-
+class TH1;
+class TH2;
 class Likelihood;
 class LikelihoodRatio;
 
@@ -18,8 +20,14 @@ public:
   virtual void monitor( LikelihoodRatio& );
 
 private:
+  void init();
+
   std::string _folder;
   std::string _ext;
+
+  std::map<double,TH2*> _likelihoods;
+  std::map<double,TH2*> _likelihoodRatios;
+  std::map<double,TH1*> _minimizedAlpha;
 
 };
 
--- src/TestStatMonitor.cxx
+++ src/TestStatMonitor.cxx
@@ -1,17 +1,40 @@
 #include "TestStatMonitor.hpp"
+#include <cmath>
 #include <string>
 
+#include <boost/assign/list_of.hpp>
+#include <boost/foreach.hpp>
+#define foreach BOOST_FOREACH
+
+#include "TH1.h"
+#include "TH2.h"
+
 using namespace std;
+using namespace boost::assign;
 
 TestStatMonitor::TestStatMonitor()
   : _folder("figures/")
   , _ext   (".png") {
+  init();
 }
 
 
 TestStatMonitor::TestStatMonitor( const string& folder, const string& ext )
   : _folder( folder )
   , _ext   ( ext ) {
+  init();
+}
+
+
+void
+TestStatMonitor::init() {
+
+  vector<double> alphas = list_of(0)             (1/pow(8000,2)) (1/pow(7000,2)) (1/pow(6000,2))
+                                 (1/pow(5000,2)) (1/pow(4000,2)) (1/pow(3000,2)) (1/pow(1500,2))
+                                 (1/pow(1000,2)) (1/pow( 750,2)) (1/pow( 500,2));
+  _likelihoods     [alpha] = new TH2D("","",100,0,-1,100,0,-1);
+  _likelihoodRatios[alpha] = new TH2D("","",100,0,-1,100,0,-1);
+  _minimizedAlpha  [alpha] = new TH1D("","",100,alphas.front(),alphas.back());
 }
 
 
@@ -21,6 +44,9 @@ TestStatMonitor::~TestStatMonitor() {
 
 void
 TestStatMonitor::monitor( Likelihood& l ) {
+
+  
+
 }
 
 
