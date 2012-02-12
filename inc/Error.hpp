62e2bbc2b7c45da6acc19d3b0070744213ac74ee
Frank Berghaus <frank@uvic.ca>
2012-02-12 13:18:29 -0800

    created skeleton for error interface and statistical error

--- inc/Error.hpp
+++ inc/Error.hpp
@@ -4,12 +4,43 @@
  *  Created on: 2012-02-12
  *      Author: frank
  */
-
 #ifndef ERROR_HPP
 #define ERROR_HPP
+#include <map>
 
+#include <TMatrixTSym.h>
+#include <TRandom3.h>
 
+class TF1;
 
+// error application interface
+struct Error {
+
+  Error() {
+  }
+  virtual ~Error() {
+  }
+
+  virtual double apply( const double&, const double&, const double&, const double& ) = 0;
+
+};
+
+// Error implementation for statical effect
+class Statitical_Error: public Error {
+public:
+  Statitical_Error();
+  Statitical_Error( const TF1*, const std::map< double, TMatrixTSym< double > >& );
+  virtual ~Statitical_Error();
+
+  virtual double apply( const double&, const double&, const double&, const double& );
+
+private:
+
+  TF1 * _fitFunction;
+  std::map< double, TMatrixTSym< double > > _covarianceMaticies;
+
+  TRandom3 _random;
+
+};
 
 #endif // ERROR_HPP
-
--- src/Error.cxx
+++ src/Error.cxx
@@ -7,6 +7,31 @@
 
 #include "Error.hpp"
 
+#include <TF1.h>
 
+using namespace std;
 
+Statitical_Error::Statitical_Error() :
+    _fitFunction( 0 ) {
+}
+
+Statitical_Error::Statitical_Error( const TF1* fitFunction,
+                                    const map< double, TMatrixTSym< double > >& covarianceMaticies ) :
+    _fitFunction( (TF1*) fitFunction->Clone( "fitFunction" ) ),
+    _covarianceMaticies( covarianceMaticies ) {
+
+}
+
+Statitical_Error::~Statitical_Error() {
+  delete _fitFunction;
+}
+
+double Statitical_Error::apply( const double& mu, const double& lambda, const double& chi, const double& mjj ) {
+
+  double result = mu;
+
+  // do stuff to result.
+
+  return result;
+}
 
